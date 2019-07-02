92489f6e05e5f7df8eb23d6945316c94a355e663
flologicshilpa <40564281+flologicshilpa@users.noreply.github.com>
2019-07-02 18:15:48 +0530

    dsfdsf

--- bot.js
+++ bot.js
@@ -90,143 +90,14 @@ function getLeaveBalance(session,val){
 
             console.log(response.body);
             
-            // var sendMessage = new builder.Message(session);
-            // var attachments = [];
+            var sendMessage = new builder.Message(session);
+            var attachments = [];
             
-            // var attachments=getCardsAttachmentsForLeavetype(response.body);
-            // sendMessage.attachments(attachments);
-            // session.send(sendMessage);
-            // session.endDialog();
-            var card = {
-                "contentType": "application/vnd.microsoft.card.adaptive",
-                "content": {
-                    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-                    "type": "AdaptiveCard",
-                    "version": "1.0",
-                    "body": [
-                        {
-                            "type": "Container",
-                            "items": [
-                                {
-                                    "type": "TextBlock",
-                                    "text": "Publish Adaptive Card schema",
-                                    "weight": "bolder",
-                                    "size": "medium"
-                                },
-                                {
-                                    "type": "ColumnSet",
-                                    "columns": [
-                                        {
-                                            "type": "Column",
-                                            "width": "auto",
-                                            "items": [
-                                                {
-                                                    "type": "Image",
-                                                    "url": "https://pbs.twimg.com/profile_images/3647943215/d7f12830b3c17a5a9e4afcc370e3a37e_400x400.jpeg",
-                                                    "size": "small",
-                                                    "style": "person"
-                                                }
-                                            ]
-                                        },
-                                        {
-                                            "type": "Column",
-                                            "width": "stretch",
-                                            "items": [
-                                                {
-                                                    "type": "TextBlock",
-                                                    "text": "Matt Hidinger",
-                                                    "weight": "bolder",
-                                                    "wrap": true
-                                                },
-                                                {
-                                                    "type": "TextBlock",
-                                                    "spacing": "none",
-                                                    "text": "Created {{DATE(2017-02-14T06:08:39Z, SHORT)}}",
-                                                    "isSubtle": true,
-                                                    "wrap": true
-                                                }
-                                            ]
-                                        }
-                                    ]
-                                }
-                            ]
-                        },
-                        {
-                            "type": "Container",
-                            "items": [
-                                {
-                                    "type": "TextBlock",
-                                    "text": "Now that we have defined the main rules and features of the format, we need to produce a schema and publish it to GitHub. The schema will be the starting point of our reference documentation.",
-                                    "wrap": true
-                                },
-                                {
-                                    "type": "FactSet",
-                                    "facts": [
-                                        {
-                                            "title": "Board:",
-                                            "value": "Adaptive Card"
-                                        },
-                                        {
-                                            "title": "List:",
-                                            "value": "Backlog"
-                                        },
-                                        {
-                                            "title": "Assigned to:",
-                                            "value": "Matt Hidinger"
-                                        },
-                                        {
-                                            "title": "Due date:",
-                                            "value": "Not set"
-                                        }
-                                    ]
-                                }
-                            ]
-                        }
-                    ],
-                    "actions": [
-                        {
-                            "type": "Action.ShowCard",
-                            "title": "Set due date",
-                            "card": {
-                                "type": "AdaptiveCard",
-                                "body": [
-                                    {
-                                        "type": "Input.Date",
-                                        "id": "dueDate"
-                                    }
-                                ],
-                                "actions": [
-                                    {
-                                        "type": "Action.Submit",
-                                        "title": "OK"
-                                    }
-                                ]
-                            }
-                        },
-                        {
-                            "type": "Action.ShowCard",
-                            "title": "Comment",
-                            "card": {
-                                "type": "AdaptiveCard",
-                                "body": [
-                                    {
-                                        "type": "Input.Text",
-                                        "id": "comment",
-                                        "isMultiline": true,
-                                        "placeholder": "Enter your comment"
-                                    }
-                                ],
-                                "actions": [
-                                    {
-                                        "type": "Action.Submit",
-                                        "title": "OK"
-                                    }
-                                ]
-                            }
-                        }
-                    ]
-                }
-            }
+            var attachments=getCardsAttachmentsForLeavetype(response.body);
+            sendMessage.attachments(attachments);
+            session.send(sendMessage);
+            session.endDialog();
+          
         
             var msg = new builder.Message()
                 .addAttachment(card)
@@ -250,88 +121,88 @@ function setResponse(session, val) {
 
 
 
-// function getCardsAttachmentsForLeavetype(data)
-// {
-//     var attachments=[];
-//     var i;
-//     console.log(data.length);
+function getCardsAttachmentsForLeavetype(data)
+{
+    var attachments=[];
+    var i;
+    console.log(data.length);
     
 
-//     for(i=0;i<data.length;i++)
-//     {      
-//         var card = {
-//             'contentType': 'application/vnd.microsoft.card.adaptive',
-//             'content': {
-//                 "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-//                 "type": "AdaptiveCard",
-//                 "version": "1.0",
-//                 "body": [                    
-//                     {
-//                         "type": "Container",
-//                         "items": [
-//                             {
-//                                 "type": "ColumnSet",
-//                                 "columns": [
-//                                     {
-//                                         "type": "Column",
-//                                         "items": [
-//                                             {
-//                                                 "type": "TextBlock",
-//                                                 "size": "Medium",
-//                                                 "weight": "Bolder",
-//                                                 "text": "Available Leave Balance",
-//                                                 "wrap": true
-//                                             }
-//                                         ],
-//                                         "width": "auto"
-//                                     }
+    for(i=0;i<data.length;i++)
+    {      
+        var card = {
+            'contentType': 'application/vnd.microsoft.card.adaptive',
+            'content': {
+                "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+                "type": "AdaptiveCard",
+                "version": "1.0",
+                "body": [                    
+                    {
+                        "type": "Container",
+                        "items": [
+                            {
+                                "type": "ColumnSet",
+                                "columns": [
+                                    {
+                                        "type": "Column",
+                                        "items": [
+                                            {
+                                                "type": "TextBlock",
+                                                "size": "Medium",
+                                                "weight": "Bolder",
+                                                "text": "Available Leave Balance",
+                                                "wrap": true
+                                            }
+                                        ],
+                                        "width": "auto"
+                                    }
 
-//                                 ] }
-//                             ]
-//                     },
-//                     {
-//                         "type": "Container",
-//                         "seprator":"true",
-//                         "items": [
-//                             {
-//                             "type": "ColumnSet",
-//                             "columns": [
-//                                 {
-//                                     "type": "Column",
-//                                     "items": [
-//                                         {
-//                                             "type": "TextBlock",
-//                                             "size": "Medium",
-//                                             "weight": "Bolder",
-//                                             "text": "test",
+                                ] }
+                            ]
+                    },
+                    {
+                        "type": "Container",
+                        "seprator":"true",
+                        "items": [
+                            {
+                            "type": "ColumnSet",
+                            "columns": [
+                                {
+                                    "type": "Column",
+                                    "items": [
+                                        {
+                                            "type": "TextBlock",
+                                            "size": "Medium",
+                                            "weight": "Bolder",
+                                            "text": "test",
                                             
-//                                         },
+                                        },
                                     
-//                                     ],
-//                                     "width": 3
-//                                 },
-//                                 {
-//                                     "type": "Column",
-//                                     "items": [
-//                                         {
-//                                             "type": "TextBlock",
-//                                             "size": "Medium",                                           
-//                                             "text": "30",
+                                    ],
+                                    "width": 3
+                                },
+                                {
+                                    "type": "Column",
+                                    "items": [
+                                        {
+                                            "type": "TextBlock",
+                                            "size": "Medium",                                           
+                                            "text": "30",
                                         
-//                                         }
-//                                     ],
-//                                     "width": 7
-//                                 }
+                                        }
+                                    ],
+                                    "width": 7
+                                }
                            
-//                             ] }
-//                         ]
-//                     }  
-//                 ]//body close
+                            ] }
+                        ]
+                    }  
+                ]//body close
           
-//             }//content
-//             };
-//         attachments.push(card); 
-//     }
-//     return attachments;
-// }
+            }//content
+            };
+        attachments.push(card); 
+    }
+    return attachments;
+}
     
